1aecfde1af7b26c091470140eb148ab2e7b7c16e
prostopolinka <130893732+prostopolinka@users.noreply.github.com>
2023-04-23 15:00:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,34 @@ class GameSprite(sprite.Sprite):
         super().__init__()
 
         # каждый спрайт должен хранить свойство image - изображение
-        self.image = transform.scale(image.load(player_image), (wight, height))  # вместе 55,55 - параметры
+        self.images = transform.scale(image.load(player_image), (wight, height))  # вместе 55,55 - параметры
         self.speed = player_speed
 
         # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-        self.rect = self.image.get_rect()
+        self.rect = self.images.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
 
     def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        window.blit(self.images, (self.rect.x, self.rect.y))
 
 
 # класс главного игрока
 class Player(GameSprite):
     # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_UP] and self.rect.x < 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+        if keys[K_DOWN] and self.rect.x < win_height - 150:
             self.rect.x += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y < 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
 
 
 # Игровая сцена:
@@ -41,11 +48,19 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 
+# создания мяча и ракетки
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)  # при созданни спрайта добавляется еще два параметра
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 # флаги отвечающие за состояние игры
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+speed_x = 2
+speed_y = 2
 
 while game:
     for e in event.get():
@@ -53,6 +68,28 @@ while game:
             game = False
     if not finish:
         window.fill(back)
+
+        if ball.rect.y <= 0 or ball.rect.y >= win_height - 50:
+            speed_y *= -1
+
+        if sprite.collide_rect(ball, racket1) or sprite.collide_rect(ball, racket2):
+            speed_x *= -1
+
+        if ball.rect.x <= 0 or ball.rect.x >= win_width - 50:
+            finish = True
+
+
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
 
     display.update()
     clock.tick(FPS)
